03ebe0467f381bad45ea36d856b98dade831a063
rwilhel <rwilhel@gmail.com>
2018-07-15 19:12:16 -0600

    Creates getUser method

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -18,5 +19,24 @@ export class UsersComponent implements OnInit {
       data => this.users$ = data
     );
   }
+}
 
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit {
+
+  users$: Object;
+
+  constructor(private route: ActivatedRoute, private data: DataService) {
+    this.route.params.subscribe( params => this.user$ = params.id );
+  }
+
+  ngOnInit() {
+    this.data.getUser(this.user$).subscribe(
+      data => this.user$ = data
+    );
+  }
 }
