d6e3a90911070a2543d178ef04eaea39498dbd24
rwilhel <rwilhel@gmail.com>
2018-07-15 19:31:09 -0600

    Moved getUser method to details component where it belongs

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Observable } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -17,26 +16,6 @@ export class UsersComponent implements OnInit {
   ngOnInit() {
     this.data.getUsers().subscribe(
       data => this.users$ = data
-    );
-  }
-}
-
-@Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
-})
-export class DetailsComponent implements OnInit {
-
-  users$: Object;
-
-  constructor(private route: ActivatedRoute, private data: DataService) {
-    this.route.params.subscribe( params => this.user$ = params.id );
-  }
-
-  ngOnInit() {
-    this.data.getUser(this.user$).subscribe(
-      data => this.user$ = data
     );
   }
 }
